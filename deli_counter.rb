1b531dfa4fdc690dd68af355f167dbf54263dd11
Sean Martin <sean.martin@gmail.com>
2018-09-19 00:19:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+
+def line(deli)
+  if deli.empty?
+end
+
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.size - 1} in line."
+end
+
+def now_serving(deli)
+  #
+end
+
