07d79f082bd512eda1c868b92be9624191c8e7c0
Sean Martin <sean.martin@gmail.com>
2018-09-19 00:24:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(deli, name)
   deli.push(name)
-  puts "Welcome, #{name}. You are number #{deli.size - 1} in line."
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
 end
 
 def now_serving(deli)
