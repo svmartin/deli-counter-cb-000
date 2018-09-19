ba959d9599b516eb9b4de0cd7659d50f90aa01ae
Sean Martin <sean.martin@gmail.com>
2018-09-19 00:21:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,11 @@ katz_deli = []
 
 def line(deli)
   if deli.empty?
+    puts "The line is currently empty."
+  else
+    message = "The line is currently: "
+    deli.each_with_index { |customer, i| message += "#{i+1}. #{customer}"}
+    puts message
 end
 
 def take_a_number(deli, name)
@@ -13,4 +18,3 @@ end
 def now_serving(deli)
   #
 end
-
